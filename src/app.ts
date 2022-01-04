4e57767564fe2d866bffa1aba51805117773566a
Kim Junsu <sbn04043@gmail.com>
2022-01-04 21:51:23 +0900

    데이터베이스 기능 추가

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import express from "express";
-import { createServer, Server } from "http";
+import { createServer } from "http";
+
+import controller from "./controller";
+import database from "./config/database";
 
 const app = express();
+
+database.sync({
+  alter: true,
+});
+app.use(express.json());
+app.use(controller);
 
 const server = createServer(app);
 server.listen(process.env.PORT || 5000);
